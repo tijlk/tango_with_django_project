e4bce7b53c3fdde632502285ae227e17c3b5a4cb
Tijl Kindt <tijl.kindt@gmail.com>
2014-12-27 20:54:48 +0100

    URL mapping added

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says hello world! Go to the <a href='/rango/about/'>about</a> page.")
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page. Go back to the <a href='/rango/'>index</a> page.")

09e6a4d6bf1b263081e258832f3e548ee9011d07
Tijl Kindt <tijl.kindt@gmail.com>
2014-12-27 22:47:49 +0100

    Fixed bug with the media url patterning

--- rango/urls.py
+++ rango/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls import patterns, url
 from rango import views
 
@@ -6,10 +5,3 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about')
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'media/(?P<path>.*)',
-        'serve',
-         {'document_root': settings.MEDIA_ROOT}), )

656cd37dc8ee724ba47c07fe02dfc1a734f7b2cb
Tijl Kindt <tijl.kindt@gmail.com>
2014-12-27 22:40:31 +0100

    First template created and used

--- rango/urls.py
+++ rango/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
 from rango import views
 
@@ -5,3 +6,11 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about')
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
